6d240e9b5147a736cff3fb35f9ec5eaafc8f7ee8
Gerard Fontarnau Galea <gerard.fontarnau.galea@gmail.com>
2022-11-16 11:29:02 +0100

    Cambio en el Program de ClassLib

--- ClassLib/Program.cs
+++ ClassLib/Program.cs
@@ -13,7 +13,8 @@ namespace ClassLib
 
             Console.WriteLine("Hello World!");
             uapCat21 testuapcat21 = new uapCat21();
-            string path = "C:/Users/sersa/Desktop/ejemplos_PGTA/Ficheros_asterix/201002-lebl-080001_adsb.ast";
+            //string path = "C:/Users/sersa/Desktop/ejemplos_PGTA/Ficheros_asterix/201002-lebl-080001_adsb.ast";
+            string path = "C:/Users/gerar/OneDrive/Escriptori/Uni/201002-lebl-080001_adsb.ast";
             AsterixFile achPrueba = new AsterixFile();
             achPrueba.setFilepath(path);
             achPrueba.readFile();
@@ -21,6 +22,11 @@ namespace ClassLib
             var cat21Item = listCat21[0];
             cat21Item.decodeDataFields();
 
+            const string getTypeArg = "FlightDataLib.ACASResolutionAdvisoryReport, FlightDataLib";
+            var createInsArg = Type.GetType(getTypeArg);
+            List<string> content = new List<string>();
+            var emptyDataField = Activator.CreateInstance(createInsArg, cat21Item);
+            Console.WriteLine(emptyDataField);
             //AsterixFile Asterix = new AsterixFile();
             //Asterix.setFilepath(path);
             //Asterix.readFile();
